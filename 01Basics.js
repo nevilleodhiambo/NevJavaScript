1f81fcd7a29726fcd86733793786130da9484060
neville <nevilleodhiambo@gmail.com>
2024-04-02 12:26:27 +0300

    Learning JavasCript

---
+++ 01Basics.js
@@ -0,0 +1,10 @@
+var loggedCount = 34;
+console.log(loggedCount);
+console.log("loggedCount");
+
+var PaymentMode;
+
+console.log(PaymentMode);
+
+PaymentMode = "Credit Card";
+console.log(PaymentMode);
